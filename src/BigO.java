172706c1442f6726c0e3ee0979f3789c14f4d0db
nicoalejo <nicoalejor@gmail.com>
2023-05-08 12:59:25 -0500

    Anadidas varias funciones

--- src/BigO.java
+++ src/BigO.java
@@ -5,14 +5,94 @@ public class BigO {
    public String O1(){
        Stack<Integer> testStack = new Stack<>();
 
-       long linearStartTime = System.nanoTime();
+       long timeStack = System.nanoTime();
        testStack.push(1);
-       long linearEndTime = System.nanoTime();
-       long linearDuration = linearEndTime - linearStartTime;
+       long endTimeStack = System.nanoTime();
+       long durationSmallStack = (endTimeStack - timeStack) / 1000;
 
+       Stack<Integer> testLongStack = new Stack<>();
 
+       for (int i=0; i < 100000; i++){
+           testLongStack.push(i);
+       }
+
+       long timeStack2 = System.nanoTime();
+       testStack.push(100005);
+       long endTimeStack2 = System.nanoTime();
+       long durationLongStack = (endTimeStack2 - timeStack2) / 1000;
+
+       return "Duracion Stack Pequeño ms: " + durationSmallStack + "\n" +
+               "Duracion Stack Grande ms: " + durationLongStack + "\n";
    }
 
+   public String hashTest(){
+       HashMap<Integer, String> testHashMap = new HashMap<>();
+       List<Integer> mylist = new ArrayList<>();
+
+       for (int i=0; i < 10000000; i++){
+           testHashMap.put(i, "num"+i);
+           mylist.add(i);
+       }
+
+       long timeinit = System.nanoTime();
+       testHashMap.get(500000);
+       long endTime = System.nanoTime();
+       long duration = (endTime - timeinit);
+
+       long timeinitList = System.nanoTime();
+       for (int i=0; i < mylist.size(); i++){
+           if(i == 500000) break;
+       }
+       long endTimeList = System.nanoTime();
+       long durationList = (endTimeList - timeinitList);
+
+       return "Duración de busqueda en el hashmap " + duration + "\n" +
+               "Duración de busqueda lineal " + durationList;
+   }
+
+   public String ForVSForEach(){
+       String timeToTraverse = "";
+       List<Integer> mylist = new ArrayList<>();
+
+       for (int i = 0; i < 10000000; i++) {
+           mylist.add(i);
+       }
+
+       long forLoopStartTime = System.currentTimeMillis();
+       for (int i = 0; i < mylist.size(); i++)
+       {
+           mylist.get(i);
+       }
+       long forLoopTraversalCost = System.currentTimeMillis()-forLoopStartTime;
+       timeToTraverse += "El tiempo que demora usando for es " + forLoopTraversalCost + "\n";
+
+       long forEachStartTime = System.currentTimeMillis();
+       for (Integer integer : mylist) {}
+
+       long forEachTraversalCost =System.currentTimeMillis()-forEachStartTime;
+       timeToTraverse += "El tiempo que demora usando for-each es " + forEachTraversalCost;
+
+       return timeToTraverse;
+   }
+
+
+   public String ON(){
+       int[] sizes = {1000, 10000, 100000, 1000000, 10000000};
+       String timeResults = "";
+       for (int size : sizes) {
+           long resultado = 0;
+           long linearStartTime = System.nanoTime();
+           for(int i=1; i <= size; i++){
+               resultado += i;
+           }
+           long linearEndTime = System.nanoTime();
+           long linearDuration = (linearEndTime - linearStartTime) / 1000000;
+
+           timeResults += "La duración del tamaño " + size + " es " + linearDuration +"\n";
+       }
+
+       return timeResults;
+   }
 
    public void TestBigO(){
         int[] sizes = {1000, 10000, 100000, 1000000, 10000000}; // Diferentes tamaños de datos para probar los algoritmos
--- src/mainForm.java
+++ src/mainForm.java
@@ -6,8 +6,11 @@ public class mainForm extends JFrame {
     private JTabbedPane tabbedPane1;
     private JPanel mainPanel;
     private JButton bigOComparisonButton;
-    private JTextArea textArea1;
+    private JTextArea textAreaResults;
     private JButton o1Button;
+    private JButton oNButton;
+    private JButton hashO1Button;
+    private JButton forVsForEachButton;
 
     private BigO bigO;
 
@@ -16,7 +19,25 @@ public class mainForm extends JFrame {
         o1Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                bigO.
+                textAreaResults.setText(bigO.O1());
+            }
+        });
+        oNButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textAreaResults.setText(bigO.ON());
+            }
+        });
+        hashO1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textAreaResults.setText(bigO.hashTest());
+            }
+        });
+        forVsForEachButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textAreaResults.setText(bigO.ForVSForEach());
             }
         });
     }
